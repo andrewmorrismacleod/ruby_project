430ca670dfe2355da7fece156ba2dfb8667134f7
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-12-14 16:21:19 +0000

    Adding film views

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 require_relative('controllers/actors_controller')
+require_relative('controllers/films_controller')
 also_reload( '../models/*' )
 
 get '/' do
---
+++ controllers/films_controller.rb
@@ -0,0 +1,15 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative( '../models/film.rb' )
+also_reload( '../models/*' )
+
+get '/films' do
+  @films = Film.all
+  erb( :"films/index")
+end
+
+get '/films/:id' do
+  @film = Film.find(params['id'].to_i)
+  erb( :"films/show")
+end
