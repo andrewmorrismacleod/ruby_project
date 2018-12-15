37d7ca94e6bdf402bfd80480762ed575cb06d9fa
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-12-15 15:00:18 +0000

    Adding delete films functionality

--- controllers/films_controller.rb
+++ controllers/films_controller.rb
@@ -19,3 +19,8 @@ post '/films/:film_id/:actor_id/delete' do
   Casting.delete_by_actor_film_id(params[:actor_id].to_i, params[:film_id].to_i)
   redirect to("/films/#{params[:film_id].to_i}")
 end
+
+post '/films/:film_id/delete' do
+  Film.delete(params[:film_id].to_i)
+  redirect to("/films")
+end
