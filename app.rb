a3da534199ceb64d476db1c424b670ec81ebcd8d
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-12-18 10:21:03 +0000

    Adding to styling elements

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require( 'sinatra/contrib/all' )
 require_relative('controllers/actors_controller')
 require_relative('controllers/films_controller')
 require_relative('controllers/generators_controller')
-also_reload( '../models/*' )
+also_reload( './*' )
 
 get '/' do
   erb( :index )
